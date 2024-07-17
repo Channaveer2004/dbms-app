5d933b7aed81d6b05f3d15cea0cc86efb88dc9dc
Channaveer <channaveerbs0905@gmail.com>
2024-07-17 21:09:59 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [datetime, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions,setTransactions] = useState([]);
+  
+  useEffect(()=>{
+    getTransaction().then(setTransactions)
+  })
+
+  async function getTransaction(){
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return await response.json();
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    let url = process.env.REACT_APP_API_URL + "/transaction";
+    const price = parseFloat(name.split(" ")[0]);
+    const actualName = name.substring(name.indexOf(" ") + 1);
+    console.log('Fetching from URL:', url);
+
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        name: actualName,
+        description, 
+        datetime,
+        price
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log('Transaction details:', json);
+        setName(" ");
+        setDate(" ");
+        setDescription(" ")
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  let balance =0;
+  for(const transaction of transactions){
+    balance = balance + transaction.price;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <main>
+        <h1>₹{balance}</h1>
+        {/* {transactions.length} */}
+        <form onSubmit={handleSubmit}>
+          <div className="basic">
+            <input
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder="Add expense"
+            />
+            <input
+              type="datetime-local"
+              onChange={e => setDate(e.target.value)}
+              value={datetime}
+            />
+          </div>
+
+          <div className="description">
+            <input
+              type="text"
+              placeholder="description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Add new transaction</button>
+        </form>
+
+        <div className="transactions">
+      {transactions.length > 0 && transactions.map(transaction => (
+        <div className="transaction" 
+key={transaction.id}>
+          <div className="left">
+            <div className="name">{transaction.name}</div>
+            <div className="description">{transaction.description}</div>
+          </div>
+          <div className="right">
+            <div className={"price " + (transaction.price < 0 ? 'red' : 'green')}>
+              {transaction.price < 0 ? `-${Math.abs(transaction.price)}` : transaction.price}
+            </div>
+          </div>
+          <div className="datetime">{transaction.datetime}</div>
+        </div>
+      ))}
     </div>
+      </main>
+    </>
   );
 }
 
